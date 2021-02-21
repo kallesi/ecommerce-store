2060caa348539fbf8f18e4fcce71a12a84a896bf
Kalle <kalle.t.sipila@gmail.com>
2021-02-22 00:22:39 +0200

    Added a contacts form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
     context = {
         "title":"Main Store Page",
@@ -16,11 +18,19 @@ def about_page(request):
     return render(request, "home_page.html", context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title":"Contact",
-        "content":"Please contact us for any enquiries"
+        "content":"Please contact us for any enquiries",
+        "form":contact_form
     }
-    return render(request, "home_page.html", context)
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method  == "POST":
+    #     print(request.POST.get("fullname"))
+    #     print(request.POST.get("email"))
+    #     print(request.POST.get("content"))
+    return render(request, "contact/view.html", context)
 
 
 
